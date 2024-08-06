923d83b1b12b1502ce7496e14fb54477dfb84c17
coderlol <xsare435@gmail.com>
2024-08-06 22:22:24 +0200

    Rearrange some files and store event listeners in event.lua

--- os/boot/02_event.lua
+++ os/boot/02_event.lua
@@ -1,13 +1,11 @@
 local process = require("process")
-local listeners = {}
+local event = require("event")
 
 local eventproc = process.create("events", function()
     while true do
         local ev = table.pack(computer.pullSignal(0))
-        -- print(ev[1])
         if ev.n ~= 0 then
-            -- print(tostring(ev))
-            for _, listener in pairs(listeners) do
+            for _, listener in pairs(event.listeners) do
                 if listener.event == ev[1] then
                     listener.func(table.unpack(ev, 2, #ev-1))
                 end 
@@ -16,7 +14,3 @@ local eventproc = process.create("events", function()
         coroutine.yield()
     end
 end)
-
-function _addEventListener(event, func)
-    listeners[#listeners+1] = {["event"]=event, ["func"]=func}
-end
--- os/lib/event.lua
+++ os/lib/event.lua
@@ -1,25 +1,8 @@
 local event = {}
+event.listeners = {}
 
-function event.listen(event, func)
-    _addEventListener(event, func)
+function event.listen(_event, func)
+    event.listeners[#event.listeners+1] = {["event"]=_event, ["func"]=func}
 end
-
--- function os.sleep(timeout)
---     checkArg(1, timeout, "number", "nil")
---     local deadline = computer.uptime() + (timeout or 0)
---     repeat
---         -- computer.pullSignal(0)
---         coroutine.yield()
---     until computer.uptime() >= deadline
--- end
-
--- function event.timeout(event, timeout)
---     local deadline = computer.uptime() + timeout
---     while computer.uptime() < deadline do
---         coroutine.yield()
---         local data = computer.pullSignal(0)
---         if data ~= nil then break end
---     end
--- end
 
 return event
